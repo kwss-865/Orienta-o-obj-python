8a3af56d96cab707f89cb1961b16862245f61adf
kwss-865 <134401209+kwss-865@users.noreply.github.com>
2024-09-05 12:10:38 -0300

    Add files via upload

---
+++ modelos/proprietario.py
@@ -0,0 +1,12 @@
+class Proprietario:
+    nome = ''
+    imovel = ''
+    localizao = ''
+    contrato = False
+    
+proprietario_Ricardo = Proprietario()
+proprietario_Roberta = Proprietario()
+
+proprietarios = [proprietario_Ricardo, proprietario_Roberta]
+
+print(proprietarios)
