a8a1c64bfc95b9efab5a245e68355bba892f40ed
kwss-865 <134401209+kwss-865@users.noreply.github.com>
2024-09-11 11:55:48 -0300

    Add files via upload

--- modelos/proprietario.py
+++ modelos/proprietario.py
@@ -1,12 +1,19 @@
 class Proprietario:
     nome = ''
+    nome_completo = ''
     imovel = ''
     localizao = ''
     contrato = False
     
 proprietario_Ricardo = Proprietario()
 proprietario_Roberta = Proprietario()
+proprietario_Lia = Proprietario()
 
-proprietarios = [proprietario_Ricardo, proprietario_Roberta]
+proprietario_Roberta.nome_completo = 'Roberta Brazza'
+proprietario_Ricardo.nome_completo = 'Ricardo Bezera'
 
-print(proprietarios)
+proprietarios = [proprietario_Ricardo, proprietario_Roberta, proprietario_Lia]
+
+print(proprietarios\n)
+print(proprietario_Roberta\n)
+print(dir(proprietarios))
