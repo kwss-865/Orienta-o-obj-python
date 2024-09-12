12789846804d68685bc0ddbd036f96175fb78103
kwss-865 <134401209+kwss-865@users.noreply.github.com>
2024-09-12 12:09:38 -0300

    Add files via upload

--- modelos/proprietario.py
+++ modelos/proprietario.py
@@ -1,19 +1,23 @@
 class Proprietario:
-    nome = ''
-    nome_completo = ''
-    imovel = ''
-    localizao = ''
-    contrato = False
+    def __init__(self, nome, nome_completo, imovel, localizacao):
+        self.nome = nome
+        self.nome_completo = nome_completo
+        self.imovel = imovel
+        self.localizao = localizacao
+        self.contrato = False
     
-proprietario_Ricardo = Proprietario()
-proprietario_Roberta = Proprietario()
-proprietario_Lia = Proprietario()
-
-proprietario_Roberta.nome_completo = 'Roberta Brazza'
-proprietario_Ricardo.nome_completo = 'Ricardo Bezera'
+    def __str__(self):
+        return f'{self.nome} | {self.nome_completo} | {self.imovel} | {self.localizao} | {self.contrato}'
+    
+proprietario_Ricardo = Proprietario('Ricardo', 'Ricardo Brazza', 'Apartamento', 'Edificio José Maria')
+proprietario_Roberta = Proprietario('Roberta', 'Roberta Bezera', 'Casa', 'Rua Roberto batata 1190')
+proprietario_Lia = Proprietario('Lia', 'Lia Mutsuki', 'Casa', 'Rua Lion Ribeiro 2385')
 
 proprietarios = [proprietario_Ricardo, proprietario_Roberta, proprietario_Lia]
 
-print(proprietarios\n)
-print(proprietario_Roberta\n)
-print(dir(proprietarios))
+print(proprietario_Ricardo)
+print(proprietario_Lia)
+
+
+
+
